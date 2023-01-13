fd1b64e4e542f546cd6bfb37baad689cd4c1c023
Caitlin Croteau <cjcroteau@gmail.com>
2023-01-12 17:40:07 -0800

    add Navigation component

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import logo from "./images/JRICH-logo.png";
 import CompanyDetails from "./components/CompanyDetails";
 import Services from "./components/Services";
 import Form from "./components/Form";
+import Navigation from "./components/Navigation";
 
 function App() {
   return (
     <div className="App">
+      <Navigation />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
       </header>
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,3 @@
+export default function Navigation(props) {
+  return <div>Navigation goes here.</div>;
+}
