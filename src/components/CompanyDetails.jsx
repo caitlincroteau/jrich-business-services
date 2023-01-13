e2ffc2083280720d291ad3ed5fd46e9852012ea2
Caitlin Croteau <cjcroteau@gmail.com>
2023-01-12 17:58:05 -0800

    set index styles

--- src/components/CompanyDetails.jsx
+++ src/components/CompanyDetails.jsx
@@ -1,18 +1,16 @@
-
 export default function CompanyDetails(props) {
   return (
     <div>
-    <p>
-      JRich Business Services Inc. is a company dedicated to providing you with efficient business solutions adapted to your specific needs. With 33 years at Canada Revenue Agency, Janis Richards has the experience to assist you in managing your finances for a better bottom line.
-    </p>
-    <p>
-      To get started with our services, please fill out our online questionnaire below or contact us via phone or
-      email.
-    </p>
-  </div>
-
+      <div>
+        JRich Business Services Inc. is a company dedicated to providing you
+        with efficient business solutions adapted to your specific needs. With
+        33 years at Canada Revenue Agency, Janis Richards has the experience to
+        assist you in managing your finances for a better bottom line.
+      </div>
+      <div>
+        To get started with our services, please fill out our online
+        questionnaire below or contact us via phone or email.
+      </div>
+    </div>
   );
-
-
-
 }
