15ebd4a1264903cb47ad6c72f5fc626c751ceeaa
Caitlin Croteau <cjcroteau@gmail.com>
2023-01-16 13:20:57 -0800

    add grid to Intro

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,16 +1,24 @@
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
 export default function Intro(props) {
   return (
-    <div>
-      <div class="text">
-        JRich Business Services Inc. is a company dedicated to providing you
-        with efficient business solutions adapted to your specific needs. With
-        33 years at Canada Revenue Agency, Janis Richards has the experience to
-        assist you in managing your finances for a better bottom line.
-      </div>
-      <div class="text">
-        To get started with our services, please fill out our online
-        questionnaire below or contact us via phone or email.
-      </div>
-    </div>
+    <Container fluid="md" className="text align-text center">
+      <Row className="pb-3">
+        <Col>
+          JRich Business Services Inc. is a company dedicated to providing you
+          with efficient business solutions adapted to your specific needs. With
+          33 years at Canada Revenue Agency, Janis Richards has the experience
+          to assist you in managing your finances for a better bottom line.
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          To get started with our services, please fill out our online
+          questionnaire below or contact us via phone or email.
+        </Col>
+      </Row>
+    </Container>
   );
 }
