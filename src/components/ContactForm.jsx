5e5754dafa903f7942ce403cce1330cfbc786772
Caitlin Croteau <cjcroteau@gmail.com>
2023-01-16 14:19:29 -0800

    clean up layout on ContactForm

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -6,31 +6,38 @@ import Button from "react-bootstrap/Button";
 
 export default function ContactForm(props) {
   return (
-    <Container>
+    <Container className="p-3 ms-auto square border border-2 rounded text-start card-lrg-bkgrnd">
       <Form>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>First Name</Form.Label>
-          <Form.Control type="text" placeholder="First Name" />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Last Name</Form.Label>
-          <Form.Control type="text" placeholder="Last Name" />
-        </Form.Group>
+        <Row className="mb-4 mt-4">
+          <Form.Group as={Col} controlId="formBasicEmail">
+            <Form.Label>First Name</Form.Label>
+            <Form.Control type="text" placeholder="First Name" />
+          </Form.Group>
+          <Form.Group as={Col} controlId="formBasicEmail">
+            <Form.Label>Last Name</Form.Label>
+            <Form.Control type="text" placeholder="Last Name" />
+          </Form.Group>
+        </Row>
+
+        <Row>
+          <Form.Group as={Col} className="mb-3" controlId="formBasicEmail">
+            <Form.Label>City</Form.Label>
+            <Form.Control type="text" placeholder="City" />
+          </Form.Group>
+
+          <Form.Group as={Col} className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Phone</Form.Label>
+            <Form.Control type="text" placeholder="Phone" />
+          </Form.Group>
+        </Row>
+
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
           <Form.Control type="email" placeholder="Enter email" />
         </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>City</Form.Label>
-          <Form.Control type="text" placeholder="City" />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Phone</Form.Label>
-          <Form.Control type="text" placeholder="Phone" />
-        </Form.Group>
+
         <Form.Group className="mb-3" id="formGridCheckbox">
           <Form.Label>What services does your business provide?</Form.Label>
-
           <Form.Check type="checkbox" label="Contractors/Trades" />
           <Form.Check type="checkbox" label="Health and Wellness Therapists" />
           <Form.Check type="checkbox" label="Performing and Visual Artists" />
@@ -56,6 +63,7 @@ export default function ContactForm(props) {
             Please check as many as apply.
           </Form.Text>
         </Form.Group>
+
         <Form.Group className="mb-3">
           <Form.Label>
             Do you have a current bookkeeper or accountant?
@@ -73,10 +81,12 @@ export default function ContactForm(props) {
             id="formHorizontalRadios1"
           />
         </Form.Group>
+
         <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
           <Form.Label>Questions or Comments:</Form.Label>
           <Form.Control as="textarea" />
         </Form.Group>
+
         <Button variant="dark" type="submit">
           Submit
         </Button>
