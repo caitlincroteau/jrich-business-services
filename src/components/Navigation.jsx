1016ef95ba5a8b6c28b0938cd8b1cea2b8087b31
Caitlin Croteau <cjcroteau@gmail.com>
2023-01-17 14:35:36 -0800

    update Nav Brand

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,12 +1,20 @@
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import Brand from "../images/JRICH-name.png";
 
 export default function Navigation(props) {
   return (
     <Navbar bg="light" expand="lg" fixed="top">
       <Container>
-        <Navbar.Brand href="#home">JRich</Navbar.Brand>
+        <Navbar.Brand href="#home">
+          <img
+            alt=""
+            width="100"
+            src={Brand}
+            className="d-inline-block align-top"
+          />
+        </Navbar.Brand>
         <Nav className="me-auto">
           <Nav.Link href="#services">Services</Nav.Link>
           <Nav.Link href="#contact">Contact</Nav.Link>
