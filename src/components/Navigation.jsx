177d94cf6e10f1cfe410c83d61c7e662542c002e
Caitlin Croteau <cjcroteau@gmail.com>
2023-01-17 13:50:19 -0800

    basic Navigation layout

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,3 +1,17 @@
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+
 export default function Navigation(props) {
-  return <div class="text">Navigation goes here.</div>;
+  return (
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="#home">JRich</Navbar.Brand>
+        <Nav className="me-auto">
+          <Nav.Link href="#services">Services</Nav.Link>
+          <Nav.Link href="#contact">Contact</Nav.Link>
+        </Nav>
+      </Container>
+    </Navbar>
+  );
 }
