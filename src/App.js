f82cc7c42de44038124bb29b2a0e26f92f1c8751
Caitlin Croteau <cjcroteau@gmail.com>
2023-01-17 18:37:01 -0800

    remove old form commponent

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import "./styles/App.css";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 import logo from "./images/JRICH-brand-med-transp.png";
 import CompanyDetails from "./components/CompanyDetails";
 
-import Form from "./components/Form";
 import Navigation from "./components/Navigation";
 
 function App() {
--- src/components/Form.jsx
+++
@@ -1,3 +0,0 @@
-export default function Form(props) {
-  return <div class="text">Form goes here.</div>;
-}
