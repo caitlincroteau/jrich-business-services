535b1407f0afa4c71e3db7a50161acfe63c2d603
Caitlin Croteau <cjcroteau@gmail.com>
2023-01-17 19:15:33 -0800

    add contact icons and fix alignment

--- src/components/ContactInfo.jsx
+++ src/components/ContactInfo.jsx
@@ -1,18 +1,35 @@
-import Container from "react-bootstrap/Container"
+import Container from "react-bootstrap/Container";
+import EmailIcon from "../images/JRICH-icon-email.png";
+import PhoneIcon from "../images/JRICH-icon-phone.png";
+import AddressIcon from "../images/JRICH-icon-address.png";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 export default function ContactInfo(props) {
-  return(
-    <Container>
+  return (
+    <Container fluid="md" className="text p-3" id="contact">
       <h1>Contact Info</h1>
-      <h2>Address:</h2>
-      <p>Suite 109, 3-11 Bellrose Drive
-St. Albert, Alberta
-T8N 5C9
-</p>
-<p>jerich@shaw.ca
-780-995-2807
-</p>
-
+      <Row className="p-1">
+        <Col md="auto">
+          {" "}
+          <img alt="" width="30px" height="30px" src={AddressIcon} />
+        </Col>
+        <Col md="auto"> Suite 109, 3-11 Bellrose Drive, St. Albert, Alberta T8N 5C9</Col>
+      </Row>
+      <Row className="p-1">
+        <Col md="auto">
+          {" "}
+          <img alt="" width="30px" height="30px" src={EmailIcon} />
+        </Col>
+        <Col md="auto">jerich@shaw.ca</Col>
+      </Row>
+      <Row className="p-1">
+        <Col md="auto">
+          {" "}
+          <img alt="" width="30px" height="30px" src={PhoneIcon} />
+        </Col>
+        <Col md="auto">780-995-2807</Col>
+      </Row>
     </Container>
-  )
+  );
 }
