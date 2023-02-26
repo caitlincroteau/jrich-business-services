4def5e8ab0abd971cc5b10d064997b79c6996682
Caitlin Croteau <cjcroteau@gmail.com>
2023-02-25 21:49:18 -0800

    change logo size

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,12 @@ import logo from "../images/JRICH-brand-med-transp.png";
 export default function Header(props) {
   return (
     <Container className="App-header">
-      <img src={logo} className="pt-4 pb-2" alt="logo" />
+      <img
+        src={logo}
+        className="pt-4 pb-2"
+        alt="logo"
+        style={{ width: "380px" }}
+      />
     </Container>
   );
 }
