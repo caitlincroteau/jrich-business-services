493778d41608b6f822410c972c010e74ac1301f4
Caitlin Croteau <cjcroteau@gmail.com>
2023-02-25 21:34:47 -0800

    create Header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import "./styles/App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import logo from "./images/JRICH-brand-med-transp.png";
+
 import Intro from "./components/Intro";
 import Services from "./components/Services";
 import Footer from "./components/Footer";
-
+import Header from "./components/Header";
 import Navigation from "./components/Navigation";
 import ContactInfo from "./components/ContactInfo";
 
@@ -12,9 +12,7 @@ function App() {
   return (
     <div className="App" id="home">
       <Navigation />
-      <header className="App-header">
-        <img src={logo} className="pt-4 pb-2" alt="logo" />
-      </header>
+      <Header />
       <Intro />
       <Services />
       <ContactInfo />
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import Container from "react-bootstrap/Container";
+import logo from "../images/JRICH-brand-med-transp.png";
+
+export default function Header(props) {
+  return (
+    <Container className="App-header">
+      <img src={logo} className="pt-4 pb-2" alt="logo" />
+    </Container>
+  );
+}
